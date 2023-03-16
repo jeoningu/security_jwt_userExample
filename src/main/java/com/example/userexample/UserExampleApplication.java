05c2cd19fc8861eae2978df830d3528753f591e5
jeoningu <doremipasolsol1@gmail.com>
2023-03-16 14:53:02 +0900

    Initial commit

---
+++ src/main/java/com/example/userexample/UserExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.userexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userexample/UserExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
